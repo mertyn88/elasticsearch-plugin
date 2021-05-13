b778fe243b7734d14f8b3c2f9798b2fdf4398121
이준명 <mertyn@homeplus.co.kr>
2021-05-13 14:05:01 +0900

    + TestPrintKoreanAnalyzer 작성 + UserDic 테스

--- nori-analyzer/src/test/java/org/apache/lucene/analysis/ko/print/TestPrintKoreanAnalyzer.java
+++ nori-analyzer/src/test/java/org/apache/lucene/analysis/ko/print/TestPrintKoreanAnalyzer.java
@@ -19,10 +19,7 @@ package org.apache.lucene.analysis.ko.print;
 import java.io.IOException;
 import java.util.Arrays;
 import java.util.HashSet;
-import java.util.Random;
 import java.util.Set;
-import java.util.stream.Collectors;
-import org.apache.lucene.analysis.Analyzer;
 import org.apache.lucene.analysis.BaseTokenStreamTestCase;
 import org.apache.lucene.analysis.TokenStream;
 import org.apache.lucene.analysis.ko.KoreanAnalyzer;
@@ -82,77 +79,32 @@ public class TestPrintKoreanAnalyzer extends BaseTokenStreamTestCase {
       NNP | Proper Noun  | 고유 명사 | 한국(NNP)
      */
     Set<POS.Tag> stopTags = new HashSet<>(Arrays.asList(POS.Tag.NNG, POS.Tag.NNP, POS.Tag.JX));
-    try(TokenStream ts = new KoreanAnalyzer(null, KoreanTokenizer.DecompoundMode.DISCARD, stopTags, false)
+    try(TokenStream ts = new KoreanAnalyzer(null, KoreanTokenizer.DecompoundMode.NONE, stopTags, false)
         .tokenStream("dummy", "한국은 대단한 나라입니다.")){
       analyzer(ts);
     }
   }
 
-
-  public void testStopTags() throws IOException {
-    Set<POS.Tag> stopTags = Arrays.asList(POS.Tag.NNP, POS.Tag.NNG).stream().collect(Collectors.toSet());
-    Analyzer a = new KoreanAnalyzer(null, KoreanTokenizer.DecompoundMode.DISCARD, stopTags, false);
-    assertAnalyzesTo(a, "한국은 대단한 나라입니다.",
-        new String[]{"은", "대단", "하", "ᆫ", "이", "ᄇ니다"},
-        new int[]{ 2, 4, 6, 6, 10, 10 },
-        new int[]{ 3, 6, 7, 7, 13, 13 },
-        new int[]{ 2, 1, 1, 1, 2, 1 }
-    );
-    a.close();
+  public void test_unknown_word_outputUnknownUnigrams_true() throws IOException {
+    // outputUnknownUnigrams true -> 참이면 알 수없는 단어에 대한 유니 그램을 출력합니다.
+    // [2018] -> [2],[0],[1],[8]
+    try(TokenStream ts = new KoreanAnalyzer(null, KoreanTokenizer.DecompoundMode.NONE, KoreanPartOfSpeechStopFilter.DEFAULT_STOP_TAGS, true)
+        .tokenStream("dummy", "2018 평창 동계올림픽대회")){
+      analyzer(ts);
+    }
+  }
+  public void test_unknown_word_outputUnknownUnigrams_false() throws IOException {
+    try(TokenStream ts = new KoreanAnalyzer(null, KoreanTokenizer.DecompoundMode.NONE, KoreanPartOfSpeechStopFilter.DEFAULT_STOP_TAGS, false)
+        .tokenStream("dummy", "2018 평창 동계올림픽대회")){
+      analyzer(ts);
+    }
   }
 
-  public void testUnknownWord() throws IOException {
-    Analyzer a = new KoreanAnalyzer(null, KoreanTokenizer.DecompoundMode.DISCARD,
-        KoreanPartOfSpeechStopFilter.DEFAULT_STOP_TAGS, true);
-
-    assertAnalyzesTo(a,"2018 평창 동계올림픽대회",
-        new String[]{"2", "0", "1", "8", "평창", "동계", "올림픽", "대회"},
-        new int[]{0, 1, 2, 3, 5, 8, 10, 13},
-        new int[]{1, 2, 3, 4, 7, 10, 13, 15},
-        new int[]{1, 1, 1, 1, 1, 1, 1, 1});
-    a.close();
-
-    a = new KoreanAnalyzer(null, KoreanTokenizer.DecompoundMode.DISCARD,
-        KoreanPartOfSpeechStopFilter.DEFAULT_STOP_TAGS, false);
-
-    assertAnalyzesTo(a,"2018 평창 동계올림픽대회",
-        new String[]{"2018", "평창", "동계", "올림픽", "대회"},
-        new int[]{0, 5, 8, 10, 13},
-        new int[]{4, 7, 10, 13, 15},
-        new int[]{1, 1, 1, 1, 1});
-    a.close();
-  }
-
-  /**
-   * blast random strings against the analyzer
-   */
-  public void testRandom() throws IOException {
-    Random random = random();
-    final Analyzer a = new KoreanAnalyzer();
-    checkRandomData(random, a, atLeast(1000));
-    a.close();
-  }
-
-  /**
-   * blast some random large strings through the analyzer
-   */
-  public void testRandomHugeStrings() throws Exception {
-    Random random = random();
-    final Analyzer a = new KoreanAnalyzer();
-    checkRandomData(random, a, 2 * RANDOM_MULTIPLIER, 8192);
-    a.close();
-  }
-
-  // Copied from TestKoreanTokenizer, to make sure passing
-  // user dict to analyzer works:
-  public void testUserDict() throws IOException {
-    final Analyzer analyzer = new KoreanAnalyzer(TestKoreanTokenizer.readDict(),
-        KoreanTokenizer.DEFAULT_DECOMPOUND, KoreanPartOfSpeechStopFilter.DEFAULT_STOP_TAGS, false);
-    assertAnalyzesTo(analyzer, "c++ 프로그래밍 언어",
-        new String[]{"c++", "프로그래밍", "언어"},
-        new int[]{0, 4, 10},
-        new int[]{3, 9, 12},
-        new int[]{1, 1, 1}
-    );
+  public void test_user_dict() throws IOException {
+    try(TokenStream ts = new KoreanAnalyzer(TestKoreanTokenizer.readDict(),
+        KoreanTokenizer.DEFAULT_DECOMPOUND, KoreanPartOfSpeechStopFilter.DEFAULT_STOP_TAGS, true)
+        .tokenStream("dummy", "2018년도 세종, 2019년도 세종시")){
+      analyzer(ts);
+    }
   }
 }
