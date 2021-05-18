977720193b2ca176cc78b203d1d5beff6ffb7af4
이준명 <mertyn@homeplus.co.kr>
2021-05-18 16:56:01 +0900

    + 테스트 네이밍 변경

--- nori-plugin/src/test/java/org/elasticsearch/index/analysis/NoriDefaultAnalyzerTest.java
+++ nori-plugin/src/test/java/org/elasticsearch/index/analysis/NoriDefaultAnalyzerTest.java
@@ -1,4 +1,4 @@
-package org.elasticsearch.index.analysis.tokenizer;
+package org.elasticsearch.index.analysis;
 
 import java.io.IOException;
 import java.io.StringReader;
@@ -15,7 +15,7 @@ import org.elasticsearch.plugin.analysis.nori.NoriDefaultPlugin;
 import org.elasticsearch.test.ESTestCase;
 import org.junit.Test;
 
-public class NoriDefaultTokenizerTest extends ESTestCase {
+public class NoriDefaultAnalyzerTest extends ESTestCase {
 
     @Test
     public void test_nori_default_tokenizer() throws IOException {
